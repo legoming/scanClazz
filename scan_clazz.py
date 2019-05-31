ab3479cc93962700a0745e70cec8b62e9aa5683c
Jet CHEN <legoming@gmail.com>
2019-05-31 14:57:53 +0800

    reimplement code with tree structure

--- scan_clazz.py
+++ scan_clazz.py
@@ -4,6 +4,8 @@ import os
 import sys
 import re
 
+from tree import TreeNode, dump
+
 KEYWORD_PUBLIC = r"public"
 KEYWORD_ABSTRACT = r"(abstract\ +)?"
 KEYWORD_CLASS = r"class"
@@ -32,67 +34,76 @@ def __init__():
     pass
 
 
-def draw_class_relationship(dict_class_parent, list_class_def, dict_class_reliedclass, set_class_not_standalone):
-    set_defined_classes = set()
-    if len(dict_class_parent) > 0:
+def draw_class_relationship(dict_class_parent, dict_class_reliedclass, dict_classname_treenode, key_class):
+    if dict_classname_treenode is not None and len(dict_classname_treenode) >0:
         fo = open('output', 'w')
         #fo.write('```graphviz')
         fo.write('\ndigraph G {')
+        #fo.write('\nrankdir = LR')
+
+        haskey = True if key_class is not None and key_class in dict_classname_treenode.keys() else False
+        key_nd = dict_classname_treenode.get(key_class)
+
+        for cls in dict_classname_treenode:
+            nd = dict_classname_treenode.get(cls)
+            if nd is not None and nd.is_valid_node():
+                if nd.is_standalone():
+                    print('drop standalone ' + nd.name)
+                elif nd.is_equal(key_class):
+                    fo.write('\n    ' + nd.displayname + '[shape = egg color=green]')
+                elif nd.is_parent():
+                    print('parent node ' + nd.name)
+                    fo.write('\n    ' + nd.displayname + '[shape = component]')
+                elif nd.is_leaf():
+                    fo.write('\n    ' + nd.displayname + '[shape = plaintext]')
+                else:
+                    fo.write('\n    ' + nd.displayname + '[shape = note]')
         if dict_class_parent is not None and len(dict_class_parent) > 0:
             for cls in dict_class_parent:
                 if cls is not None and dict_class_parent[cls] is not None:
-                    cls_converted = cls.replace('.', '・').replace('<', '‹').replace('>', '›')
-                    pnt_converted = dict_class_parent[cls].replace('.', '・').replace('<', '‹').replace('>', '›')
-                    if cls not in set_defined_classes:
-                        fo.write('\n    ' + cls_converted + '[shape = note]')
-                        set_defined_classes.add(cls)
-                    if dict_class_parent[cls] not in set_defined_classes:
-                        fo.write('\n    ' + pnt_converted + '[shape = component]')
-                        set_defined_classes.add(dict_class_parent[cls])
+                    cls_converted = dict_classname_treenode.get(cls).displayname
+                    pnt_converted = dict_classname_treenode.get(dict_class_parent[cls]).displayname
                     fo.write('\n    ' + cls_converted + ' -> ' + pnt_converted + '[arrowhead = empty color=purple]')
-                    fo.write('\n')
-        if list_class_def is not None and len(list_class_def) > 0:
-            if cls is not None:
-                for cls in list_class_def:
-                    if cls not in set_defined_classes and cls in set_class_not_standalone:
-                        cls_converted = cls.replace('.', '・').replace('<', '‹').replace('>', '›')
-                        fo.write('\n    ' + cls_converted + '[shape = note]')
-                        set_defined_classes.add(cls)
-                    elif cls not in set_class_not_standalone:
-                        print(cls + ' is standalone' )
         if dict_class_reliedclass is not None and len(dict_class_reliedclass) > 0:
             for cls in dict_class_reliedclass:
                 if cls is not None:
-                    cls_converted = cls.replace('.', '・').replace('<', '‹').replace('>', '›')
+                    cls_converted = dict_classname_treenode.get(cls).displayname
                     if dict_class_reliedclass is not None:
                         for relatedcls in dict_class_reliedclass.get(cls):
                             if relatedcls != cls:
-                                relatedcls_converted = relatedcls.replace('.', '・').replace('<', '‹').replace('>', '›')
-                                fo.write('\n    ' + cls_converted + ' -> ' + relatedcls_converted + '[style = dashed]')
+                                relatedcls_converted = dict_classname_treenode.get(relatedcls).displayname
+                                if haskey and \
+                                        (key_nd.is_clz_direct_relate_with_node(cls) or
+                                         key_nd.is_clz_direct_relate_with_node(relatedcls)):
+                                    fo.write('\n    ' + cls_converted + ' -> ' + relatedcls_converted + '[style = dashed]')
+                                elif haskey:
+                                    fo.write('\n    ' + cls_converted + ' -> ' + relatedcls_converted + '[style = dashed color = gray]')
+                                else:
+                                    fo.write('\n    ' + cls_converted + ' -> ' + relatedcls_converted + '[style = dashed]')
         fo.write('\n}')
         #fo.write('\n```')
         fo.close()
 
 
-def scan_class_define(root_dir, mode, excluded_class):
+def scan_class_define(root_dir, mode, excluded_class, key_class):
     dict_filename_classname = {}
     dict_class_parent = {}
     list_class_def = []
     dict_class_reliedclass = {}
     set_class_not_standalone = set()
+
+    dict_classname_treenode = {}
     for root, subdirs, files in os.walk(root_dir):
+        print(files)
         for filename in files:
             if filename.find('.java') > 0:
                 filepath = os.path.join(root, filename)
                 f = open(filepath, 'r')
-
-                set_importedclass = set()
-
+                classname = ''
                 for line in f:
                     if line.startswith('import'):
                         pass
                     elif re.match(PATTERN_CLASS_WITH_PARENT, line):
-                        #print(line)
                         try:
                             classname = re.search(KEYWORD_CLASS + '(.*)' + KEYWORD_EXTEND, line).group(1).strip()
                             parentname = re.search(KEYWORD_EXTEND + '(.*)', line).group(1).lstrip()
@@ -104,53 +115,97 @@ def scan_class_define(root_dir, mode, excluded_class):
                                 parentname = parentname[:parentname.index(r'{')]
                             except:
                                 pass
-                            if len(classname) > 0 and len(parentname) > 0 \
-                                    and classname not in excluded_class \
-                                    and parentname not in excluded_class:
-                                dict_class_parent[classname] = parentname
+                            should_link = True
+                            if len(classname) > 0 and classname not in excluded_class:
                                 dict_filename_classname[filename] = classname
+                                if classname not in dict_classname_treenode.keys():
+                                    nd = TreeNode(classname)
+                                    dict_classname_treenode[classname] = nd
+                            else:
+                                should_link = False
+                            if len(parentname) > 0 and parentname not in excluded_class:
+                                if parentname not in dict_classname_treenode.keys():
+                                    nd = TreeNode(parentname)
+                                    dict_classname_treenode[parentname] = nd
+                            else:
+                                should_link = False
+                            if should_link:
+                                dict_class_parent[classname] = parentname
                                 set_class_not_standalone.add(classname)
                                 set_class_not_standalone.add(parentname)
-                        except:
-                            pass
+
+                                dict_classname_treenode.get(classname).add_parent(parentname)
+                                dict_classname_treenode.get(parentname).add_child(classname)
+
+                        except Exception as e:
+                            print('PATTERN_CLASS_WITH_PARENT except\n\t' + str(e))
                         break
                     elif re.match(PATTERN_CLASS_DEFINE, line):
                         classname = re.search(KEYWORD_CLASS + '(.*)', line).group(1).strip()
                         try:
                             classname = classname[:classname.index(r' ')]
-                        except:
-                            pass
-                        if classname not in excluded_class:
+                        except Exception as e:
+                            print('PATTERN_CLASS_DEFINE except\n\t' + str(e))
+                        if len(classname) > 0 and classname not in excluded_class:
                             list_class_def.append(classname)
                             dict_filename_classname[filename] = classname
+                            if classname not in dict_classname_treenode.keys():
+                                nd = TreeNode(classname)
+                                dict_classname_treenode[classname] = nd
                         break
                 f.close()
-    if len(list_class_def) > 0 and mode.find('r') >= 0:
+    print('='*20)
+    for filename in dict_filename_classname:
+        print(filename + ' : ' + dict_filename_classname[filename])
+    print('=' * 20)
+    if len(dict_classname_treenode) > 0 and mode.find('r') >= 0:
         for root, subdirs, files in os.walk(root_dir):
+            print(files)
             for filename in files:
                 if filename.find('.java') > 0:
                     filepath = os.path.join(root, filename)
-                    print(filename + ' : ' + filepath)
+                    print('parsing '+ filename + ' : ' + filepath)
                     f = open(filepath, 'r')
                     buff = f.read()
                     f.close()
-                    set_importedclass = set()
+                    set_reliedclass = set()
 
-                    for clz in list_class_def:
-                        pat = r"\ " + clz + r"\.|new\ " + clz
+                    fclass = dict_filename_classname.get(filename)
+
+                    if fclass is None:
+                        print('should not happen ' + filename)
+                        continue
+
+                    if fclass not in dict_classname_treenode:
+                        nd = TreeNode(fclass)
+                        dict_classname_treenode[fclass] = nd
+                    nd_fclass = dict_classname_treenode.get(fclass)
+
+                    for clz in dict_classname_treenode:
+                        pat = r"\ " + clz + r"\.|new\ " + clz + r"|\"[a-zA-Z\.]+" + clz + r"\""
+                        #print(pat)
                         # \ Intent\.|new Intent
                         if re.search(pat, buff):
-                            set_importedclass.add(clz)
+                            set_reliedclass.add(clz)
                             set_class_not_standalone.add(clz)
-                    dict_class_reliedclass[dict_filename_classname.get(filename)] = set_importedclass
-                    if len(set_importedclass) > 0:
-                        set_class_not_standalone.add(dict_filename_classname.get(filename))
+                            # clz's node has created already
+                            nd_clz = dict_classname_treenode.get(clz)
+                            print('\t find relied class ' + nd_clz.name)
+                            nd_clz.add_lchild(fclass)
+                            nd_fclass.add_rchild(clz)
+                    dict_class_reliedclass[fclass] = set_reliedclass
+                    if len(set_reliedclass) < 1:
+                        print('\t no relied class')
+
+                    if len(set_reliedclass) > 0:
+                        set_class_not_standalone.add(fclass)
+
     #print(dict_class_reliedclass)
-    draw_class_relationship(dict_class_parent, list_class_def, dict_class_reliedclass, set_class_not_standalone)
+    draw_class_relationship(dict_class_parent, dict_class_reliedclass, dict_classname_treenode, key_class)
 
 
-def main(root_dir, mode, excluded_class):
-    scan_class_define(root_dir, mode, excluded_class)
+def main(root_dir, mode, excluded_class, key_class):
+    scan_class_define(root_dir, mode, excluded_class, key_class)
 
 
 if __name__ == '__main__':
@@ -161,6 +216,7 @@ if __name__ == '__main__':
                 #   - 'i' : interface
                 #   - 'r' : rely
     excluded_class = []
+    key_class = None
     if len(sys.argv) > 1:
         for i in range(1, len(sys.argv)):
             argv = sys.argv[i].strip()
@@ -180,6 +236,12 @@ if __name__ == '__main__':
                     print(excluded_class)
                 except:
                     pass
+            elif argv == '-k': # key class wanted to observe
+                try:
+                    key_class = sys.argv[i + 1]
+                except:
+                    pass
+
     else:
         # test only
         #main('/Users/lego/workspace/omadm') # scanning in current directory
@@ -187,7 +249,7 @@ if __name__ == '__main__':
         #main('/Users/lego/aosp/packages/apps/Settings/src')
 
     if root_dir is not None:
-        main(root_dir, mode, excluded_class)
+        main(root_dir, mode, excluded_class, key_class)
     else:
         print('pls assign root dir to scan with -p')
     exit(0)
---
+++ tree.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+
+
+debug = False
+
+def print_debug(str):
+    if debug:
+        print(str)
+
+class TreeNode:
+    def __init__(self, classname):
+        self.parent = None # class name
+        self.lchild = set() # set of class name, who relied on current class
+        self.rchild = set() # set of class name, whom current class is relied on
+        self.childs = set() # set of class name, who is inherited from current class
+        self.name = classname.strip()
+        self.displayname = classname.strip().replace('.', '・').replace('<', '‹').replace('>', '›')
+
+    def is_valid_node(self):
+        return True if self.name is not None and len(self.name) > 0 else False
+
+    def is_equal(self, classname):
+        return True if classname == self.name else False
+
+    def is_parent(self):
+        return True if len(self.childs) > 0 else False
+
+    def is_leaf(self):
+        return True if len(self.childs) < 1 else False
+
+    def add_parent(self, parent_class_name):
+        # parent class of current class
+        self.parent = parent_class_name
+
+    def add_child(self, child_class_name):
+        # sometimes we don't know it's left or right child
+        self.childs.add(child_class_name)
+
+    def add_lchild(self, child_class_name):
+        # classes relied on current class
+        self.lchild.add(child_class_name)
+
+    def add_rchild(self, child_class_name):
+        # classes whom current class is relied on
+        self.rchild.add(child_class_name)
+
+    def is_standalone(self):
+        if len(self.childs) > 0 \
+                or len(self.lchild) > 0 \
+                or len(self.rchild) > 0 \
+                or self.parent is not None:
+            return False
+        else:
+            return True
+
+    def get_rootnode(self):
+        rootnode = self
+        while rootnode.parent is not None:
+            rootnode = rootnode.parent
+        return rootnode
+
+    def get_node_from_root(self, with_class_name):
+        root = self.get_rootnode()
+        return root.get_node_from_current(with_class_name)
+
+    def get_node_from_current(self, with_class_name):
+        # will goto infinite loop
+        # if with_class_name is None or len(with_class_name.strip()) < 1:
+        #     return None
+        # else:
+        #     print_debug('get_node_from_current from ' + self.name + ' for ' + with_class_name)
+        # if self.name == with_class_name:
+        #     return self
+        # elif len(self.lchild) > 0:
+        #     for nd in self.lchild:
+        #         nd.get_node_from_current(with_class_name)
+        # elif len(self.rchild) > 0:
+        #     for nd in self.rchild:
+        #         nd.get_node_from_current(with_class_name)
+        return None
+
+    def is_parent_of_node(self, clz_name):
+        return True if self.parent is not None and self.parent == clz_name else False
+
+    def is_child_of_node(self, clz_name):
+        return True if len(self.childs) > 0 and clz_name in self.childs else False
+
+    def is_rely_on_node(self, clz_name):
+        return True if len(self.lchild) > 0 and clz_name in self.lchild else False
+
+    def is_relied_by_node(self, clz_name):
+        return True if len(self.rchild) > 0 and clz_name in self.rchild else False
+
+    def is_clz_direct_relate_with_node(self, clz_name):
+        # only check direct childs and parent
+        print_debug('node class ' + self.name + ' , class to check ' + clz_name)
+        return True if self.is_parent_of_node(clz_name) \
+            or self.is_child_of_node(clz_name) \
+            or self.is_rely_on_node(clz_name) \
+            or self.is_relied_by_node(clz_name) else False
+
+
+def dump(dict_classname_treenode):
+    if dict_classname_treenode is not None and len(dict_classname_treenode) > 0:
+        for clz in dict_classname_treenode:
+            nd = dict_classname_treenode.get(clz)
+            print(nd.name)
+            if nd.parent is not None:
+                print('\t parent = ' + nd.parent)
+            else:
+                print('\t parent = none')
+            print('\t childs = ' + str(nd.childs))
+            print('\t relied by ' + str(nd.lchild))
+            print('\t relied on ' + str(nd.rchild))
+
+
