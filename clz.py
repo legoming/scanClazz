5113974cbd32d0a55a77b8e12e5a76bf147e1b41
Jet CHEN <legoming@gmail.com>
2026-02-25 15:57:33 +0800

    feat: add kotlin class scanning support and parser refactor

--- clz.py
+++ clz.py
@@ -20,6 +20,9 @@ class ClzRelationShips:
         self.argvs = {}
 
     def set_var(self, arg_type, arg_value):
+        if arg_type in ('key_class', 'key_class_id') and arg_value is None:
+            self.argvs[arg_type] = arg_value
+            return
         if arg_type in DicArgvTypes.keys() and str(type(arg_value)) == DicArgvTypes.get(arg_type):
             self.argvs[arg_type] = arg_value
         else:
@@ -32,5 +35,4 @@ class ClzRelationShips:
         else:
             print('ClzRelationShips error: ' + arg_type + ' cannot be found in argvs')
             return None
-
 
